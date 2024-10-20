4e8fe20031f23ec7dcd4f0debf863c47fbec0b9e
Blaise7 <blaisearnaud9@gmail.com>
2024-10-20 22:15:00 +0200

    added content on index.js

---
+++ styles/index.js
@@ -0,0 +1,12 @@
+// 1. Ask the user to enter a price tag
+let price = prompt("Enter the price tag (e.g., 49.99):");
+
+// 2. Convert the input from string to number
+let priceNumber = parseFloat(price);
+
+// 3. Reduce the price by 10 percent
+let discountedPrice = priceNumber * 0.9;
+
+// 4. Print out the new price
+console.log("New price: $" + discountedPrice.toFixed(2));
+alert("New price: $" + discountedPrice.toFixed(2));
